1bce72ccae1cc35f9bb9d1a51d5be74c46de2cbe
Sunil Vishwakarma <sunilnet4@gmail.com>
2021-01-04 11:31:13 +0000

    some code changes

--- index.js
+++ index.js
@@ -3,59 +3,61 @@ console.log("Answer all the questions, lets see how good you know about movies."
 console.log("-----------------------------------------------------")
 
 var readlineSync = require("readline-sync");
-score = 0;
+var score = 0;
 
-function quiz(question, answer) {
+  function quiz(question, answer) {
 
-  var userQuestion = readlineSync.question(question);
-  // console.log(userQuestion.toUpperCase);
+    var userQuestion = readlineSync.question(question);
+    // console.log(userQuestion.toUpperCase);
 
-  if (userQuestion.toUpperCase() === answer.toUpperCase()) {
+    if (userQuestion.toUpperCase() === answer.toUpperCase()) {
 
-    console.log("You're Right");
-    score++;
-  } else {
-    console.log("You're Wrong!");
+      console.log("You're Right");
+      score++;
+    } else {
+      console.log("You're Wrong!");
+    }
+    console.log("Your score is", score);
+    console.log("------------------------------------");
   }
-  console.log("Your score is", score);
-  console.log("------------------------------------");
-}
 
-var questions = [{
-  question: "Who killed Jessica? ",
-  answer: "No one"
-}, {
-  question: "When did Ajav Devgn and his family went for satsang? ",
-  answer: "2 october"
-}, {
-  question: "Mother of Superman? ",
-  answer: "Martha"
-}, {
-  question: "Batman's city? ",
-  answer: "Gotham"
-}, {
-  question: "Which radiation does Hulk is made of? ",
-  answer: "Gamma"
-}, {
-  question: "Space deployed assistant of Tony Stark? ",
-  answer: "Veronica"
-}, {
-  question: "Speed of the fastest suit of Iron man? ",
-  answer: "Mark 42"
-}
-]
+  var questions = [{
+    question: "Who killed Jessica? ",
+    answer: "No one"
+  }, {
+    question: "When did Ajav Devgn and his family went for satsang? ",
+    answer: "2 october"
+  }, {
+    question: "Mother of Superman? ",
+    answer: "Martha"
+  }, {
+    question: "Batman's city? ",
+    answer: "Gotham"
+  }, {
+    question: "Which radiation does Hulk is made of? ",
+    answer: "Gamma"
+  }, {
+    question: "Space deployed assistant of Tony Stark? ",
+    answer: "Veronica"
+  }, {
+    question: "Speed of the fastest suit of Iron man? ",
+    answer: "Mark 42"
+  }
+  ]
 
-for (var i = 0; i < questions.length; i++) {
-  quiz(questions[i].question, questions[i].answer)
-}
+  for (var i = 0; i < questions.length; i++) {
+    quiz(questions[i].question, questions[i].answer)
+  }
 
-var highScore = [{
-  name: "Sunil",
-  score: 7
-}]
+  var highScore = [{
+    name: "Sunil",
+    score: 7
+  }]
 
-if (score === highScore[0].score) {
-  console.log("You've topped the score!")
-} else if (score === 5 || 6) {
-  console.log("You're one of top scorers but missed the top spot.")
-}
+  // console.log(highScore[0].score)
+
+  if (score === highScore[0].score) {
+    console.log("You've topped the score!")
+  }else {
+    console.log("Sorry! You did'nt make it. Try Again.")
+  }
